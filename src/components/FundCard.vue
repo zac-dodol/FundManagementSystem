201e427c47b0e72d807275b814b76b0a4b1a4732
Zac Emoned <46713066+zac-dodol@users.noreply.github.com>
2023-09-11 00:15:10 +0800

    Update FundCard.vue

--- src/components/FundCard.vue
+++ src/components/FundCard.vue
@@ -6,7 +6,7 @@
       <h2 class="text-3xl">{{ fundes.fund }}</h2>
     </div>
 
-    <div class="flex flex-col gap-2 w-1/5">
+    <div class="flex flex-col gap-2 w-2/5 md:w-1/5">
       <p class="text-3xl self-end"></p>
       <div class="flex gap-2 flex-col">
         <div class="text-xs flex justify-between w-full gap-4">
